e6ac697f4535492ae645afb2092e179dcd3db4f6
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-09-22 10:48:44 +0200

    skrypt

--- analiza_txt/analiza_wykres.py
+++ analiza_txt/analiza_wykres.py
@@ -3,3 +3,8 @@ with open(plik_wejsciowy, encoding="utf-8") as plik:
     wykres = plik.readlines()
 
 print(wykres)
+naglowek = wykres[0:4]
+dane = wykres[4:]
+
+print(f"{naglowek}")
+print(f"{naglowek=}")
