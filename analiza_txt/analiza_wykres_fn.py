d87104b3a99cccddf5c3610d0f4da4f1168904fb
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-09-22 12:39:01 +0200

    req

--- analiza_txt/analiza_wykres_fn.py
+++ analiza_txt/analiza_wykres_fn.py
@@ -1,3 +1,4 @@
+import os
 import matplotlib.pyplot as plt
 
 def przygotuj_dane_do_wyswietlenia(punkty_y):
@@ -11,3 +12,19 @@ def przygotuj_dane_do_wyswietlenia(punkty_y):
     odciete_y = punkty_y[index_y:]
     elementy_x = [ x for x in range(len(odciete_y))]
     return [elementy_x, odciete_y]
+
+def wczytaj_dane_z_katalogu(nazwa_katalogu, maska=".txt"):
+    if not os.path.isdir(nazwa_katalogu):
+        return False
+
+    for dirpath, dirname, files in os.walk(nazwa_katalogu):
+        print(f"dirpath = {dirpath}")
+        print(f"dirname = {dirname}")
+        print(f"files = {files}")
+        if maska in files:
+            print(f"{maska=} / {files=}")
+
+
+# tutaj start skryptu
+katalog = "../Dane_txt"
+wczytaj_dane_z_katalogu(katalog)
