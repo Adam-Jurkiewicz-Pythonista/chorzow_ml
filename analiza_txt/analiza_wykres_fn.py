79219ebd06395fa7cece8d3430a618757ba752a3
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-09-22 12:50:15 +0200

    req

--- analiza_txt/analiza_wykres_fn.py
+++ analiza_txt/analiza_wykres_fn.py
@@ -13,20 +13,19 @@ def przygotuj_dane_do_wyswietlenia(punkty_y):
     elementy_x = [ x for x in range(len(odciete_y))]
     return [elementy_x, odciete_y]
 
-def wczytaj_dane_z_katalogu(nazwa_katalogu, maska=".txt"):
+def wczytaj_dane_z_katalogu(nazwa_katalogu, maska=".txt", min_wielkosc=100):
     if not os.path.isdir(nazwa_katalogu):
         return False
 
     for dirpath, dirname, files in os.walk(nazwa_katalogu):
-        print(f"dirpath = {dirpath}")
-        print(f"dirname = {dirname}")
-        print(f"files = {files}")
+
         for each_file in files:
             if maska in each_file:
                 plik_z_danymi = dirpath + "/" + each_file
-                print(f"{plik_z_danymi=}")
+                if os.path.getsize(plik_z_danymi)>min_wielkosc:
+                    print(f"{plik_z_danymi=}")
 
 
 # tutaj start skryptu
 katalog = "../Dane_txt"
-wczytaj_dane_z_katalogu(katalog)
+wczytaj_dane_z_katalogu(katalog,min_wielkosc=1)
