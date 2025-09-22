48a97d5fe8f4d701f776e5905b22c398ff2cb21b
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2025-09-22 11:35:22 +0200

    skrypt

--- analiza_txt/analiza_wykres.py
+++ analiza_txt/analiza_wykres.py
@@ -2,9 +2,21 @@ plik_wejsciowy = "../Dane_txt/CMSX.txt"
 with open(plik_wejsciowy, encoding="utf-8") as plik:
     wykres = plik.readlines()
 
-print(wykres)
+# print(wykres)
 naglowek = wykres[0:4]
 dane = wykres[4:]
 
-print(f"{naglowek}")
-print(f"{naglowek=}")
+# print(f"{naglowek}")
+# print(f"{naglowek=}")
+# print(f"{dane=}")
+
+elementy_y = []
+for element in dane:
+    dana = element.split(" ")
+    for liczba in dana:
+        try:
+            elementy_y.append(int(liczba))
+        except:
+            pass
+
+print(f"{elementy_y=}")
